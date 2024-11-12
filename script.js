fda1074b8ad5bcb8fcea58433c4c97506757b598
odaniellevi <daniellevi.unicap@gmail.com>
2024-11-11 23:40:41 -0300

    correcao-no-carregamento

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let progress = 0;
 const progressBar = document.querySelector(".progress");
 const loadingContainer = document.getElementById("loading-container");
+const mainContent = document.getElementById("main-content");
 
 function updateProgress() {
   if (progress < 100) {
-    progress += 1.5;
+    progress += 2;
     progressBar.style.width = progress + "%";
     setTimeout(updateProgress, 20);
   } else {
     loadingContainer.style.opacity = "0";
-    setTimeout(() => (loadingContainer.style.display = "none"), 500);
+    setTimeout(() => {
+      loadingContainer.style.display = "none";
+      mainContent.style.opacity = "1";
+    }, 500);
   }
 }
 
